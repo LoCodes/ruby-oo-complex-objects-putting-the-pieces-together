eed908ee3be47e9d3056a02beba3b97bab9c0451
Loren Cipriano <loren25cipriano@gmail.com>
2020-11-27 16:51:05 -0800

    commit tree 7d6eed0e690e197551ade09bf51f19ef10447e14

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,34 @@
 
+class Book 
+
+    attr_accessor :author, :page_count, :genre
+    attr_reader :title
+
+
+    def initialize(title)
+        @title = title
+    end 
+
+    # def properties(author, page_count, genre)
+    #     @author = author 
+    #     @page_count = page_count
+    #     @genre = genre
+    # end 
+
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end 
+end 
+
+
+# Book
+#   ::new
+#     gets initialized with a title
+#   properties
+#     has a title
+#     has an author name
+#     has a page count
+#     has a genre
+#   #turn_page
+#     can turn the page
 
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,32 @@
 # Make your shoe class here!
+
+class Shoe
+
+    attr_accessor :color, :size, :material, :condition
+    attr_reader :brand
+
+    def initialize(brand)
+        @brand = brand 
+    end
+
+    def cobble
+        puts "Your shoe is as good as new!"
+        @condition = "new"
+    end
+
+end
+
+
+# Shoe
+#   ::new
+#     gets initialized with a brand
+#   properties
+#     has a brand
+#     has a color
+#     has a size
+#     has a material
+#     has a condition
+#   #cobble
+#     says that the shoe has been repaired
+# Your shoe is as good as new!
+#     makes the shoe's condition new
